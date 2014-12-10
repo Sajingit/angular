e8c6a17d7b93d56e36687ae7cc5cb8608b537c08
Sajin Saleem <saleemsajin@gmail.com>
2014-12-10 23:43:57 +0530

    Add examples

--- examples/js/app.js
+++ examples/js/app.js
@@ -72,33 +72,32 @@
 		$scope.formDetails = [];
 		$scope.type = 'Reccuring';
 		$scope.grade = 0;
+		$scope.index = null;
 
-
-		$scope.addFormDetails = function () { alert($scope.hiddenId);
-			if($scope.hiddenId != ''){
-				$scope.formDetails.push({
-					name: $scope.name,
-					email: $scope.email,	
-					phone: $scope.phone,	
-					grade: $scope.grade,	
-					type: $scope.type,			
-				});
+		$scope.addFormDetails = function () { 
+			
+			if($scope.index != null){
+				var contacts = {
+						name: $scope.name,
+						email: $scope.email,	
+						phone: $scope.phone,	
+						grade: $scope.grade,	
+						type: $scope.type,
+					};
+			$scope.formDetails[$scope.index] = contacts;
+			$scope.index = null;
+			
 			}else{
 
-				var contacts = {
-							       name: $scope.name,
-					email: $scope.email,	
-					phone: $scope.phone,	
-					grade: $scope.grade,	
-					type: $scope.type,
-				};
-console.log(contacts);
-				$scope.formDetails[$scope.hiddenId] = contacts;
-				
-
+				$scope.formDetails.push({
+						name: $scope.name,
+						email: $scope.email,	
+						phone: $scope.phone,	
+						grade: $scope.grade,	
+						type: $scope.type,			
+				});
 			}
-
-
+			$scope.reset();
 		}
 		
 		$scope.remove = function (detail) {
@@ -107,10 +106,19 @@ console.log(contacts);
 
 		$scope.edit = function (detail) {
 			$scope.name = detail.name;
-			var index = $scope.formDetails.indexOf(detail);
-			$scope.hiddenId = index;
-			
-			//$scope.formDetails.splice($scope.formDetails.indexOf(detail),1);
+			$scope.email = detail.email;
+			$scope.phone = detail.phone;
+			$scope.grade = detail.grade;
+			$scope.type = detail.type;
+			$scope.index = $scope.formDetails.indexOf(detail);
+		}
+		
+		$scope.reset = function (){
+			$scope.name = '';
+			$scope.email = '';	
+			$scope.phone = '';	
+			$scope.grade = '';	
+			$scope.type = '';	
 		}
 	});
 	
