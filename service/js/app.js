a3811ec6ee6f312aa8948c82b36f756a4d78a851
Sajin Saleem <saleemsajin@gmail.com>
2014-12-11 21:36:03 +0530

    Add all chanegs

--- service/js/app.js
+++ service/js/app.js
@@ -2,6 +2,7 @@
 	var app = angular.module('service', []);
 	app.controller('serviceController', function( $scope ){ 
 		
+		$scope.selectedService = [];
 		$scope.services = [
 		{
 			name: 'Web Development',
@@ -36,10 +37,28 @@
 			});
 			return total;
 		}
-		$scope.alert = function (){ 
-alert("Test");
-
+		
+		$scope.addSelectedService = function (){ 
+			var nameCon = '';
+			var total = 0;
+			angular.forEach($scope.services,function(service){ 
+				if(service.active == true){
+					nameCon = nameCon + service.name + ", " ;
+					total = total + service.price;
+				}
+			});
+			
+			$scope.selectedService.push({
+				name: nameCon.trim().slice(0,-1),
+				price: total,
+			})
 		}
+		
+		$scope.remove = function (item){ console.log($scope.selectedService.indexOf(item));
+			$scope.selectedService.splice($scope.selectedService.indexOf(item),1)
+		}
+		
+		
 	});
 
 })();	 
